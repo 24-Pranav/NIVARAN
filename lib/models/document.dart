b992e093d746bfd250b40c19b103b237b9c4922a
pranavrajput024 <pranavrajput024@gmail.com>
2026-01-16 12:03:30 +0000

    changes done

--- lib/models/document.dart
+++ lib/models/document.dart
@@ -1,15 +1,15 @@
 class Document {
   final String title;
-  final String url;
+  final String? url;
   final bool isFolder;
   final bool isLink;
 
-  Document({required this.title, required this.url, this.isFolder = false, this.isLink = false});
+  Document({required this.title, this.url, this.isFolder = false, this.isLink = false});
 
   factory Document.fromJson(Map<String, dynamic> json) {
     return Document(
       title: json['title'],
-      url: json['url'],
+      url: json['url'] ?? '',
       isFolder: json['isFolder'] ?? false,
       isLink: json['isLink'] ?? false,
     );
